f7aa2e37830f313b9dee395e9ccd51c63d66fd87
Debhusi <154050869+Debhusi@users.noreply.github.com>
2023-12-22 15:23:39 +0800

    Add files via upload

---
+++ contactus.js
@@ -0,0 +1,23 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+      
+        var form = document.getElementById('form-ctn');
+        var popup = document.getElementById('popup');
+
+       
+        form.addEventListener('submit', function (event) {
+            event.preventDefault(); 
+
+           
+            popup.style.display = 'block';
+
+            
+            alert('Thank you!\nYour message has been successfully submitted. Salamat!');
+        });
+
+        
+        document.getElementById('close-btn').addEventListener('click', function () {
+            
+            popup.style.display = 'none';
+        });
+    });
